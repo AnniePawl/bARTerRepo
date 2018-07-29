20db67b6e85762a9fb5d1bdd7c607a05843ea0de
Anna Pawl <annampawl@gmail.com>
2018-07-28 17:34:09 -0700

    Next Button + Database working
    Next button on "create username" page succesfully writes to Realtime Database

--- BarterApp/Controllers/CreateUsernameViewController.swift
+++ BarterApp/Controllers/CreateUsernameViewController.swift
@@ -23,26 +23,41 @@ class CreateUsernameViewController: UIViewController {
         super.viewDidLoad()
     }
     
-
     @IBAction func nextButtonTapped(_ sender: UIButton) {
-        
-        //check that FIRUser is logged in + user has username in the text field
         guard let firUser = Auth.auth().currentUser,
             let username = usernameTextField.text,
             !username.isEmpty else { return }
         
-        UserService.create(firUser, username: username) { (user) in
-            guard let user = user else { return }
-            
-            print("Created new user: \(user.username)")
-            //new instance of main story board
-            let storyboard = UIStoryboard(name: "Home", bundle: .main)
-            //check if storyboard has initial view controller and  reference to current window and set the rootViewController to the initial view controller
-            if let initialViewController = storyboard.instantiateInitialViewController() {
-                self.view.window?.rootViewController = initialViewController
-                self.view.window?.makeKeyAndVisible()
-            }
-        }
+        let userAttrs = ["username": username]
         
+        let ref = Database.database().reference().child("users").child(firUser.uid)
+
+        ref.setValue(userAttrs) { (error, ref) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+                return
+            }
+    
+            ref.observeSingleEvent(of: .value, with: { (snapshot) in
+                let user = User(snapshot: snapshot)
+                
+            })
         }
     }
+
+//
+//
+//        UserService.create(firUser, username: username) { (user) in
+//            guard let user = user else { return }
+//
+//            print("Created new user: \(user.username)")
+//This is supposed to be in appDelegate?
+//            let storyboard = UIStoryboard(name: "Home", bundle: .main)
+//            if let initialViewController = storyboard.instantiateInitialViewController() {
+//                self.view.window?.rootViewController = initialViewController
+//                self.view.window?.makeKeyAndVisible()
+//            }
+//        }
+    
+        }
+
