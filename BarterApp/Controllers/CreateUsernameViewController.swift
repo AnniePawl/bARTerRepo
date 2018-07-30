f6b23a70a4f1834c39398476450ffd9e45c3a60f
Anna Pawl <annampawl@gmail.com>
2018-07-30 16:24:20 -0700

    Refactored Show User

--- BarterApp/Controllers/CreateUsernameViewController.swift
+++ BarterApp/Controllers/CreateUsernameViewController.swift
@@ -32,6 +32,8 @@ class CreateUsernameViewController: UIViewController {
         UserService.create(firUser, username: username) { (user) in
             guard let user = user else { return }
             
+             User.setCurrent(user)
+            
             let storyboard = UIStoryboard(name: "Home", bundle: .main)
             
             if let initialViewController = storyboard.instantiateInitialViewController() {
