1348302b3cc92a4b2f3508b623492c84df162d04
Anna Pawl <annampawl@gmail.com>
2018-08-03 15:26:56 -0700

    commented out MainTabBarController

--- BarterApp/Controllers/MainTabBarController.swift
+++ BarterApp/Controllers/MainTabBarController.swift
@@ -1,26 +1,26 @@
+////
+////  MainTabBarController.swift
+////  BarterApp
+////
+////  Created by Anna Pawl on 8/2/18.
+////  Copyright © 2018 Anna Pawl. All rights reserved.
+////
 //
-//  MainTabBarController.swift
-//  BarterApp
+//import Foundation
 //
-//  Created by Anna Pawl on 8/2/18.
-//  Copyright © 2018 Anna Pawl. All rights reserved.
+//class MainTabBarController: UITabBarController {
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    
+//        delegate = self
+//     
+//        tabBar.unselectedItemTintColor = .black
+//    }
+//}
 //
-
-import Foundation
-
-class MainTabBarController: UITabBarController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    
-        delegate = self
-     
-        tabBar.unselectedItemTintColor = .black
-    }
-}
-
-extension MainTabBarController: UITabBarControllerDelegate {
-    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
-        return true
-    }
-}
+//extension MainTabBarController: UITabBarControllerDelegate {
+//    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+//        return true
+//    }
+//}
