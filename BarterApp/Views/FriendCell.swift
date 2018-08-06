fc4891c0dbc1225ba5ed478ebf0bd9232b191d09
Anna Pawl <annampawl@gmail.com>
2018-08-06 16:04:29 -0700

    Added Label + Button to Friend Cell

---
+++ BarterApp/Views/FriendCell.swift
@@ -0,0 +1,17 @@
+//
+//  FriendCell.swift
+//  BarterApp
+//
+//  Created by Anna Pawl on 8/6/18.
+//  Copyright © 2018 Anna Pawl. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FriendsCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
