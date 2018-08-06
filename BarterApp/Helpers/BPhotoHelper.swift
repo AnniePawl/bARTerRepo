5dce8aac8e8a12f3f51cc337e53345a384414bdb
Anna Pawl <annampawl@gmail.com>
2018-08-05 23:08:53 -0700

    Image Picker
    Take photo option working

--- BarterApp/Helpers/BPhotoHelper.swift
+++ BarterApp/Helpers/BPhotoHelper.swift
@@ -17,40 +17,35 @@ class BPhotoHelper: NSObject {
     
     // MARK: - Helper Methods
     
-//    func presentImagePickerController(with sourceType: UIImagePickerControllerSourceType, from viewController: UIViewController) {
-//        let imagePickerController = UIImagePickerController()
-//        imagePickerController.sourceType = sourceType
-//
-//        viewController.present(imagePickerController, animated: true)
-//    }
+    func presentImagePickerController(with sourceType: UIImagePickerControllerSourceType, from viewController: UIViewController) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = sourceType
 
-    
-    
-   
+        viewController.present(imagePickerController, animated: true)
+    }
+
         func presentActionSheet(from viewController: UIViewController) {
             
             // 1
             let alertController = UIAlertController(title: nil, message: "Where do you want to get your picture from?", preferredStyle: .actionSheet)
             
-            // 2
+          
             if UIImagePickerController.isSourceTypeAvailable(.camera) {
-                // 3
-                let capturePhotoAction = UIAlertAction(title: "Take Photo", style: .default, handler: { action in
-                    // do nothing yet...
+                let capturePhotoAction = UIAlertAction(title: "Take Photo", style: .default, handler: { [unowned self] action in
+                    self.presentImagePickerController(with: .camera, from: viewController)
                 })
                 
-                // 4
                 alertController.addAction(capturePhotoAction)
             }
             
-            // 5
             if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-                let uploadAction = UIAlertAction(title: "Upload from Library", style: .default, handler: { action in
-                    // do nothing yet...
+                let uploadAction = UIAlertAction(title: "Upload from Library", style: .default, handler: { [unowned self] action in
+                    self.presentImagePickerController(with: .photoLibrary, from: viewController)
                 })
                 
                 alertController.addAction(uploadAction)
             }
+
             
             // 6
             let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
