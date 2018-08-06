9a24eb93d0760b9d9c4fafce84e923ea25287197
Anna Pawl <annampawl@gmail.com>
2018-08-06 00:15:49 -0700

    Prototype cell w/ identifier "PostImageCell"

--- BarterApp/Controllers/HomeViewController.swift
+++ BarterApp/Controllers/HomeViewController.swift
@@ -12,6 +12,7 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
