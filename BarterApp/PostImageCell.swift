8d80270599143a8d4137b4bb01d780d2b6c74406
Anna Pawl <annampawl@gmail.com>
2018-08-06 00:34:32 -0700

    Created PostImageCell.swift in "views" group

---
+++ BarterApp/PostImageCell.swift
@@ -0,0 +1,17 @@
+//
+//  PostImageCell.swift
+//  BarterApp
+//
+//  Created by Anna Pawl on 8/6/18.
+//  Copyright © 2018 Anna Pawl. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PostImageCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
